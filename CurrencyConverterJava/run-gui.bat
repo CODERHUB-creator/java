4aa40496cd7549eece682f8a69551d44eb4060b1
CODERHUB-creator <mm.vemavarapu@gmail.com>
2026-09-24 00:25:48 +0530

    Add Windows GUI launcher

---
+++ CurrencyConverterJava/run-gui.bat
@@ -0,0 +1,11 @@
+@echo off
+if not exist out mkdir out
+javac -d out src\currencyconverter\*.java
+if errorlevel 1 (
+    echo.
+    echo Compilation failed. Make sure Java JDK 11 or newer is installed.
+    pause
+    exit /b 1
+)
+java -cp out currencyconverter.CurrencyConverterGUI
+pause
